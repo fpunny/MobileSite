b292f1023d35d5e96675a8bbfaf4653715fd4868
Frederic Pun <frederic.pun@gmail.com>
2017-04-23 00:25:45 -0400

    Upload Files
    The purpose of this project is to do testing for all device
    compatibility, starting with the navbar

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var dropped = false;
+
+function drop() {
+    var nav = document.getElementById('nav');
+    if (dropped === false) {
+        nav.style.top = '0px';
+        dropped = true;
+    } else {
+        nav.style.top = '-126px';
+        dropped = false;
+    }
+}
